b3102ae807ffcdea42136c8cbc17e3dc6c258f29
jodalpo <jodalpo@gmail.com>
2025-12-02 16:48:45 +0900

    fix: Fix invalid DOM properties in SVG icon

--- src/components/PlayerSetup.js
+++ src/components/PlayerSetup.js
@@ -40,7 +40,7 @@ function PlayerSetup({ players, handlePlayerNameChange, addPlayer, removePlayer,
           className="bg-light-gray text-dark-gray border border-gray-300 py-2 px-4 rounded-md cursor-pointer text-base font-medium transition-all hover:bg-gray-200 flex items-center justify-center"
           onClick={resetAll}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rotate-cw w-4 h-4 mr-2"><path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8"></path><path d="M21 3v5h-5"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-rotate-cw w-4 h-4 mr-2"><path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8"></path><path d="M21 3v5h-5"></path></svg>
           전체 초기화
         </button>
         <button 
