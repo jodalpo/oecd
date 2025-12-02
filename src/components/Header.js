44cfaf5b133da51e0e784dbb6a9aa0603d72a26c
jodalpo <jodalpo@gmail.com>
2025-12-02 17:35:03 +0900

    헤더의 제목 크기를 3xl에서 2xl로 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="bg-primary text-white p-8 rounded-lg">
-      <h1 className="text-3xl font-bold mb-2">🏌️ 골프 OECD 스코어보드</h1>
+      <h1 className="text-2xl font-bold mb-2">🏌️ 골프 OECD 스코어보드</h1>
       <p className="text-base opacity-90">재밌게 칩시다~</p>
     </div>
   );
