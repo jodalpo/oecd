8838aa35eb89cf165531d71cc7d23d2a17c33255
jodalpo <jodalpo@gmail.com>
2025-12-10 15:24:03 +0900

    Updates

---
+++ _next/static/chunks/1770ad17ee76ee4e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/e4cc9c84136ad648.js",
+  "static/chunks/471a147d2f883951.js",
+  "static/chunks/turbopack-85d1c0128506a9e8.js"
+])
--- _next/static/chunks/5fdb68cf07ac4a61.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/3c11a788f7bb0c92.js",
-  "static/chunks/ee4c474764c1107b.js",
-  "static/chunks/turbopack-1644ed445c160f7b.js"
-])
