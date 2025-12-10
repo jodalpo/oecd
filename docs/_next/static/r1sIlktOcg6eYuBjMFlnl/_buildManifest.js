e5516f83d2f67ff149349e2668e79372fe328f50
jodalpo <jodalpo@gmail.com>
2025-12-10 15:47:04 +0900

    Deploy Next.js to GitHub Pages via docs folder

---
+++ docs/_next/static/r1sIlktOcg6eYuBjMFlnl/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/1770ad17ee76ee4e.js"
+  ],
+  "/_error": [
+    "static/chunks/cf15b80e89a20cb8.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
